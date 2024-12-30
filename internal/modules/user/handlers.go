8de806b3e526cfff6d669059b6dba3ef6b4cf2cb
Belenov Dmitriy <dimabelenov@gmail.com>
2024-12-30 22:51:40 +0200

    feat: add validation for creation user

--- internal/modules/user/handlers.go
+++ internal/modules/user/handlers.go
@@ -1,6 +1,9 @@
 package user
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+)
 
 type UserHandlers struct {
 	Service *UserService
@@ -9,6 +12,8 @@ type UserHandlers struct {
 func NewUserHandlers(service *UserService) *UserHandlers {
 	return &UserHandlers{Service: service}
 }
+
+var validate = validator.New()
 
 func (h *UserHandlers) GetAllUsers(c *fiber.Ctx) error {
 	users, err := h.Service.GetAllUsers()
@@ -22,6 +27,17 @@ func (h *UserHandlers) CreateUser(c *fiber.Ctx) error {
 	var input CreateUserRequest
 	if err := c.BodyParser(&input); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid input"})
+	}
+
+	if err := validate.Struct(input); err != nil {
+		errors := make(map[string]string)
+		for _, err := range err.(validator.ValidationErrors) {
+			errors[err.Field()] = err.Tag()
+		}
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":  "Validation failed",
+			"fields": errors,
+		})
 	}
 
 	user, err := h.Service.CreateUser(input)
--- internal/modules/user/models.go
+++ internal/modules/user/models.go
@@ -11,9 +11,9 @@ type User struct {
 }
 
 type CreateUserRequest struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-	Age       int    `json:"age"`
-	Password  string `json:"password"`
+	FirstName string `json:"first_name" validate:"omitempty,min=2,max=50"`
+	LastName  string `json:"last_name" validate:"omitempty,min=2,max=50"`
+	Email     string `json:"email" validate:"required,email"`
+	Age       int    `json:"age" validate:"omitempty,gte=5,lte=120"`
+	Password  string `json:"password" validate:"required,min=8"`
 }
